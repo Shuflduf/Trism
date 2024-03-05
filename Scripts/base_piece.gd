4e0f267627f8861bb2f8c018d25959365528833b
Shuflduf <shuflduf@gmail.com>
2024-03-04 19:43:02 -0700

    commit

--- Scripts/base_piece.gd
+++ Scripts/base_piece.gd
@@ -1,16 +1,6 @@
 class_name BasePiece
 extends Node3D
 
-var bounds = {
-	"left" : -7,
-	"right" : 6,
-	"down" : -21,
-}
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta):
 	if Engine.get_physics_frames() % 30 == 0:
@@ -20,17 +10,16 @@ func _physics_process(_delta):
 		move_down()
 	
 	if Input.is_action_just_pressed("left"):
-		move_side("left")
+		move_left()
+		
+	if Input.is_action_just_pressed("right"):
+		move_right()
 
 func move_down(spaces := 1):
-	if !position.y - (spaces * 2) < -21:
+	if !get_child(0).get_child(0):
 		position.y -= spaces * 2
-
-func move_side(direction : String):
-	match direction:
-		"left" : position.x -= 2
-	if direction == "left" and detect_out_bounds("left"):
-		position.x -= 2
+	else:
+		print(get_tree().root.get_child(0).spawn_random_piece())
 		
 func detect_out_bounds(direction):
 	for child in get_parent().get_children():
@@ -38,4 +27,24 @@ func detect_out_bounds(direction):
 			print("AHGGF")
 			return false
 		else:
-			return true
+			return false
+
+func move_left():
+	# Check if any child would go out of bounds with the proposed move
+	for child in get_children():
+		if child.global_position.x - 2 < bounds.left:
+			print(child.position.x)
+			return  # If any child would go out of bounds, don't move any of them
+
+	# If none would go out of bounds, proceed with the move
+	position.x -= 2
+			
+func move_right():
+	# Check if any child would go out of bounds with the proposed move
+	for child in get_children():
+		if child.global_position.x + 2 > bounds.right:
+			print(child.position.x)
+			return  # If any child would go out of bounds, don't move any of them
+
+	# If none would go out of bounds, proceed with the move
+	position.x += 2			
