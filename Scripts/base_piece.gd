46a2039fad09fef7462f4798953889d1af5f95a8
Shuflduf <shuflduf@gmail.com>
2024-03-10 22:15:57 -0600

    fix side collsion for piec

--- Scripts/base_piece.gd
+++ Scripts/base_piece.gd
@@ -7,35 +7,59 @@ var grounded = false
 func _physics_process(_delta):
 	if !grounded:
 		if Engine.get_physics_frames() % 20 == 0:
+			check_if_grounded()
+			if !grounded:
+				move_down()
+			
+		if Input.is_action_just_pressed("soft"):
 			move_down()
 		
-	if Input.is_action_just_pressed("soft"):
-		move_down()
-	
-	if Input.is_action_just_pressed("left"):
-		move_left()
-		
-	if Input.is_action_just_pressed("right"):
-		move_right()
+		if Input.is_action_just_pressed("left"):
+			move_left()
+			
+		if Input.is_action_just_pressed("right"):
+			move_right()
 
 func check_if_grounded():
-	for block in get_children():
-		print(block.get_collision_point())
-		if block.get_collision_point() < 2:
-			block.inactive()
-			grounded = true
-			
+	pass
 
 func move_down(spaces := 1):
-	#for child in get_children():
-		#if child.check_coll():
-			#return
-		
-	position.y -= spaces * 2
-		
-
+	var can_move_down = true
+	for block in get_children():
+		#print(block.get_collision_point())
+		if !block.get_collision_point():
+			grounded = true
+			spawn_new_piece()
+			set_physics_process(false)
+			disable_all_blocks()
+			can_move_down = false
+			return
+	if can_move_down:
+		position.y -= 2 * spaces
+			
 func move_left():
-	pass #TODO
+	var can_move_left = true
+	for block in get_children():
+		#print(block.get_collision_point())
+		block.left()
+		if block.get_collision_side_point() < -8:
+			can_move_left = false
+	if can_move_left:
+		position.x -= 2
 			
 func move_right():
-	pass #TODO
+	var can_move_right = true
+	for block in get_children():
+		#print(block.get_collision_point())
+		block.right()
+		if block.get_collision_side_point() > 8:
+			can_move_right = false
+	if can_move_right:
+		position.x += 2
+
+func spawn_new_piece():
+	get_parent().spawn_random_piece()
+	
+func disable_all_blocks():
+	for block in get_children():
+		block.inactive()
--- Scripts/cube.gd
+++ Scripts/cube.gd
@@ -1,17 +1,26 @@
 class_name BaseCube
 extends MeshInstance3D
 
-@onready var raycast = $RayCast3D
+@onready var downray = $DownRay
 @onready var area3d = $Area3D
+@onready var sideray = $SideRay as RayCast3D
 
-
-func _physics_process(_delta):
-	print(global_position.y - raycast.get_collision_point().y)
+func _physics_process(delta):
+	print(sideray.get_collision_point().x - global_position.x)
 
 func get_collision_point():
-	return global_position.y - raycast.get_collision_point().y
+	return global_position.y - downray.get_collision_point().y > 2
 
 func inactive():
-	raycast.enabled = false
+	downray.enabled = false
 	area3d.set_collision_layer_value(3, false)
 	area3d.set_collision_layer_value(2, true)
+
+func left():
+	sideray.target_position.x = -50
+	
+func right():
+	sideray.target_position.x = 50
+	
+func get_collision_side_point():
+	return global_position.x - sideray.get_collision_point().x
