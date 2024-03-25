bbf068e9dcec0d80e3af441dde8e61a8b752768f
Shuflduf <shuflduf@gmail.com>
2024-03-25 15:11:36 -0600

    started working on gridmap implementation

---
+++ Scripts/GridMap.gd
@@ -0,0 +1,31 @@
+extends GridMap
+
+var i_0 = [Vector2i(0,1), Vector2i(1,1), Vector2i(2,1), Vector2i(3,1)]
+var i_90 = [Vector2i(2,0), Vector2i(2,1), Vector2i(2,2), Vector2i(2,3)]
+var i_180 = [Vector2i(0,2), Vector2i(1,2), Vector2i(2,2), Vector2i(3,2)]
+var i_270 = [Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(1,3)]
+var i = [i_0, i_90, i_180, i_270]
+
+var j_0 = [Vector2i(0,0), Vector2i(0,1), Vector2i(1,1), Vector2i(2,1)]
+var j_90 = [Vector2i(2,0), Vector2i(1,0), Vector2i(1,1), Vector2i(1,2)]
+var j_180 = [Vector2i(0,1), Vector2i(1,1), Vector2i(2,1), Vector2i(2,2)]
+var j_270 = [Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(0,2)]
+var j = [j_0, j_90, j_180, j_270]
+
+var l_0 = [Vector2i(3,0), Vector2i(0,1), Vector2i(1,1), Vector2i(2,1)]
+var l_90 = [Vector2i(2,2), Vector2i(1,0), Vector2i(1,1), Vector2i(1,2)]
+var l_180 = [Vector2i(0,1), Vector2i(1,1), Vector2i(2,1), Vector2i(0,2)]
+var l_270 = [Vector2i(1,0), Vector2i(1,1), Vector2i(1,2), Vector2i(0,0)]
+var l = [l_0, l_90, l_180, l_270]
+
+var o_0 = [Vector2i(1,0), Vector2i(2,0), Vector2i(1,1), Vector2i(2,1)]
+var o_90 = [Vector2i(1,0), Vector2i(2,0), Vector2i(1,1), Vector2i(2,1)]
+var o_180 = [Vector2i(1,0), Vector2i(2,0), Vector2i(1,1), Vector2i(2,1)]
+var o_270 = [Vector2i(1,0), Vector2i(2,0), Vector2i(1,1), Vector2i(2,1)]
+var o = [o_0, o_90, o_180, o_270]
+
+var s_0 = [Vector2i(0,1), Vector2i(1,1), Vector2i(1,0), Vector2i(2,0)]
+var s_90 = [Vector2i(1,0), Vector2i(1,1), Vector2i(2,1), Vector2i(2,2)]
+var s_180 = [Vector2i(1,0), Vector2i(2,0), Vector2i(1,1), Vector2i(2,1)] #TODO
+var s_270 = [Vector2i(1,0), Vector2i(2,0), Vector2i(1,1), Vector2i(2,1)]
+var s = [s_0, s_90, s_180, s_270]
