770968dc42df2d07a13816e90eb76f607a4d4756
Shuflduf <shuflduf@gmail.com>
2024-04-16 12:53:18 -0600

    start rewriting the whole settings system
    note to self, finish tspins first

---
+++ Scripts/settings.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var current_keybinds := []
+
+# handling settings
+var arr : int
+var das : int
+var dcd : int
+var sdf : int
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
