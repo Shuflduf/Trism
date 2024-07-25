b29f9563275f54e9176435b9d30ee73b43df0497
Shuflduf <shuflduf@gmail.com>
2024-07-24 21:37:17 -0600

    actual 3d start

--- Scripts/Components/game_board_3d.gd
+++ Scripts/Components/game_board_3d.gd
@@ -3,6 +3,8 @@
 class_name GameBoard3D
 extends GridMap
 
+@export var offset: Vector3i
+
 #helper function that converts 2d values to 3d
 func convert_vec2_vec3(vec2 : Vector2i) -> Vector3i:
 	return Vector3i(vec2.x, -vec2.y, 0)
@@ -12,8 +14,6 @@ func _ready() -> void:
 		draw())
 
 func draw() -> void:
-
 	for row: int in get_parent().game.size():
-		print("draw")
 		for col: int in get_parent().game[0].size():
-			set_cell_item(Vector3i(col, row, 0), get_parent().game[row][col])
+			set_cell_item(Vector3i(col, -row, 0) + offset, get_parent().game[row][col])
