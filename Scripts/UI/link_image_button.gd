314561a2e95bc10dee7cae883cae65782c05c461
Shuflduf <shuflduf@gmail.com>
2024-07-26 21:32:53 -0600

    exports

--- Scripts/UI/link_image_button.gd
+++ Scripts/UI/link_image_button.gd
@@ -1,11 +1,10 @@
 class_name LinkImageButton
 extends TextureButton
 
-@export var url : String
+@export var url : String = "http://cow.com"
 
 func _pressed() -> void:
 	OS.shell_open(url)
 
 func _ready() -> void:
-
 	tooltip_text += url
