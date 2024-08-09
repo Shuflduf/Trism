3afaf752dcf317beb61e17dfaf7ea2499ddb9070
Shuflduf <shuflduf@gmail.com>
2024-08-08 18:15:33 -0600

    optimizing handling settings

--- Scripts/UI/pause_menu.gd
+++ Scripts/UI/pause_menu.gd
@@ -3,22 +3,14 @@ extends Control
 @onready var main_paused := $PAUSED
 @onready var settings_menu := $Settings
 @onready var controls := $Controls
+@onready var sonic: CheckButton = $Settings/VBoxContainer/Sonic
 
 @export_file("*.tscn") var main_menu : String
 
 var can_pause := true
 
-#region handling vars
-@onready var arr_slider := %ARRSlider
-@onready var arr_num := %ARRLineEdit
-@onready var das_slider := %DASSlider
-@onready var das_num := %DASLineEdit
-@onready var dcd_slider := %DCDSlider
-@onready var dcd_num := %DCDLineEdit
-@onready var sdf_slider := %SDFSlider
-@onready var sdf_num := %SDFLineEdit
-#endregion
-@onready var sonic: CheckButton = $Settings/VBoxContainer/Sonic
+@onready var sliders := [%ARRSlider, %DASSlider, %DCDSlider, %SDFSlider]
+@onready var spinboxes := [%ARRLineEdit, %DASLineEdit, %DCDLineEdit, %SDFLineEdit]
 
 signal pause_state(pause_state : bool)
 
@@ -51,15 +43,23 @@ func handle_pause(go_to_options := false) -> void:
 		return
 
 func load_handling() -> void:
-	arr_num.value = Settings.arr
-	das_num.value = Settings.das
-	dcd_num.value = Settings.dcd
-	sdf_num.value = Settings.sdf
+	#arr_num.value = Settings.arr
+	#das_num.value = Settings.das
+	#dcd_num.value = Settings.dcd
+	#sdf_num.value = Settings.sdf
 	sonic.button_pressed = Settings.sonic
 
 func _ready() -> void:
-	load_handling()
 	visible = false
+	call_deferred("load_handling")
+
+	for i: int in sliders.size():
+		sliders[i].value_changed.connect(func(value: int) -> void:
+			spinboxes[i] = value
+			Settings.set(sliders[i].get_parent().name, value))
+
+		spinboxes[i].value_changed.connect(func(value: int) -> void:
+			sliders[i] = value)
 
 func _on_open_settings_pressed() -> void:
 	main_paused.visible = false
@@ -85,40 +85,6 @@ func _on_rtx_toggled(toggled_on: bool) -> void:
 
 func _on_cinematic_toggled(toggled_on: bool) -> void:
 	Settings.cinematic_mode = toggled_on
-
-#region handling stuff
-func _on_arr_slider_value_changed(value: int) -> void:
-	Settings.arr = value
-	arr_num.value = value
-	#modify_handling.emit("ARR", value)
-
-func _on_arr_line_edit_value_changed(value: int) -> void:
-	arr_slider.value = value
-
-func _on_das_slider_value_changed(value: int) -> void:
-	Settings.das = value
-	das_num.value = value
-	#modify_handling.emit("DAS", value)
-
-func _on_das_line_edit_value_changed(value: int) -> void:
-	das_slider.value = value
-
-func _on_dcd_slider_value_changed(value: int) -> void:
-	Settings.dcd = value
-	dcd_num.value = value
-	#modify_handling.emit("DCD", value)
-
-func _on_dcd_line_edit_value_changed(value: int) -> void:
-	dcd_slider.value = value
-
-func _on_sdf_slider_value_changed(value: int) -> void:
-	Settings.sdf = value
-	sdf_num.value = value
-	#modify_handling.emit("SDF", value)
-
-func _on_sdf_line_edit_value_changed(value: int) -> void:
-	sdf_slider.value = value
-#endregion
 
 func _on_sonic_toggled(toggled_on: bool) -> void:
 	Settings.sonic = toggled_on
